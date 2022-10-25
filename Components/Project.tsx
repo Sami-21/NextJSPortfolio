de74660bafdef581b86e8675bdfea34b1bceed10
Monsif Oussaf <monsif.oussaf@gmail.com>
2022-10-25 17:00:29 +0100

    project container halfway

--- Components/Project.tsx
+++ Components/Project.tsx
@@ -16,10 +16,10 @@ const Project: React.FC<ProjectProps> = ({ name, description, url }) => {
     threshold: 0.5,
   });
 
-  return <div ref={ref} className={inView ? `${style.ProjectContainer} ${style.ProjectFadeIn}` : `${style.ProjectContainer} ${style.ProjectFadeOut}`}>
-    {/* <h3 className={inView ? `${style.ProjectContentVisible}` : `${style.ProjectContentHidden}`}> {name}</h3>
-    <p className={inView ? `${style.ProjectContentVisible}` : `${style.ProjectContentHidden}`}> {description}</p>
-    <a href={url} target="_blank" className={inView ? `${style.ProjectContentVisible}` : `${style.ProjectContentHidden}`}>visit</a> */}
+  return <div ref={ref} className={inView ? ` ${style.ProjectContainer} ${style.ProjectFadeIn}` : `${style.ProjectContainer} ${style.ProjectFadeOut}`}>
+    <h3 className={inView ? `${style.ProjectContent} ${style.ProjectContentVisible}` : ` ${style.ProjectContent} ${style.ProjectContentHidden}`}> {name}</h3>
+    <p className={inView ? `${style.ProjectContent} ${style.ProjectContentVisible}` : ` ${style.ProjectContent} ${style.ProjectContentHidden}`}> {description}</p>
+    <a href={url} target="_blank" className={inView ? `${style.ProjectContent} ${style.ProjectContentVisible}` : ` ${style.ProjectContent} ${style.ProjectContentHidden}`}>visit</a>
   </div>;
 };
 
