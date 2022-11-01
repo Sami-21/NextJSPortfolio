f75872fa954ec083deff501c728ee272cf573d00
Sami-21 <samovitch360@gmail.com>
2022-11-01 21:04:26 +0100

    Contact Section completed

---
+++ Components/ContactSection.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { InView, useInView } from "react-intersection-observer";
+import style from "../styles/ContactSection.module.css";
+import CODTextEffect from "./CODTextEffect";
+
+const ContactSection: React.FC = () => {
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
+  return (
+    <form
+      ref={ref}
+      className={`${style.ContactContainer} ${
+        inView ? style.FadeIn : style.FadeOut
+      }`}
+      action=""
+    >
+      <CODTextEffect
+        transition1="Qntmbv"
+        transition2="Pndmuw"
+        transition3="Contact"
+      />
+      <div className={style.InputContainer}>
+        <input
+          className={style.InputField}
+          type="text"
+          name="fullName"
+          id="fullName"
+          placeholder="FullName"
+        />
+        <input
+          className={style.InputField}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+        />
+      </div>
+      <div>
+        <input
+          className={style.InputField}
+          type="text"
+          name="subject"
+          id="subject"
+          placeholder="Subject"
+        />
+      </div>
+      <div>
+        <textarea
+          className={`${style.InputField} ${style.MessageField}`}
+          name="message"
+          id="message"
+          placeholder="Your Message here..."
+        ></textarea>
+      </div>
+    </form>
+  );
+};
+
+export default ContactSection;
--- Components/PortfolioContent.tsx
+++ Components/PortfolioContent.tsx
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import AboutSection from './AboutSection'
-import GlitchedText from './GlitchedText'
-import IntroText from './IntroText'
-import NavBar from './NavBar'
-import Noisebackground from './Noisebackground'
-import WorkSection from './WorkSection'
+import React, { useEffect, useState } from "react";
+import AboutSection from "./AboutSection";
+import ContactSection from "./ContactSection";
+import GlitchedText from "./GlitchedText";
+import IntroText from "./IntroText";
+import NavBar from "./NavBar";
+import Noisebackground from "./Noisebackground";
+import WorkSection from "./WorkSection";
 
 interface Props {
-    showCompDelay: number;
+  showCompDelay: number;
 }
 
 const PortfolioContent: React.FC<Props> = ({ showCompDelay }) => {
-    const [isShown, setIsShown] = useState(false);
-    useEffect(() => {
-        const timer: NodeJS.Timer = setTimeout(() => {
-            setIsShown(true);
-        }, showCompDelay);
-        return () => {
-            clearTimeout(timer);
-        };
-    }, [isShown, showCompDelay]);
-    return isShown ? (
-        < >
-            <Noisebackground />
-            {/* <NavBar />
+  const [isShown, setIsShown] = useState(false);
+  useEffect(() => {
+    const timer: NodeJS.Timer = setTimeout(() => {
+      setIsShown(true);
+    }, showCompDelay);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isShown, showCompDelay]);
+  return isShown ? (
+    <>
+      <Noisebackground />
+      {/* <NavBar />
             <IntroText />
             <AboutSection /> */}
-            <WorkSection />
-        </ >) : null
+      {/* <WorkSection /> */}
+      <ContactSection />
+    </>
+  ) : null;
+};
 
-}
-
-export default PortfolioContent
+export default PortfolioContent;
